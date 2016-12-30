44c85890b4f86efd2cf741ac8a160786b138193e
Stevenlogan <luoto735067173@163.com>
2016-12-30 21:36:24 +0800

    修改
    修改首页样式

--- index.php
+++ index.php
@@ -9,3 +9,16 @@ abstract class form
     
   }
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>OA管理系统</title>
+</head>
+<body>
+	<div>
+		<img src="">
+		<p>OA管理系统</p>
+	</div>
+</body>
+</html>
